8ca4a7d70ece88692bf630ed010a365702ac0d9e
dltk-ai <67465164+dltk-ai@users.noreply.github.com>
2020-07-19 16:24:49 +0530

    Add files via upload

---
+++ python/examples/face_licence_plate_detection.py
@@ -0,0 +1,25 @@
+import dltk_ai
+
+
+def save_img(response, filepath):
+    with open(filepath, "wb") as f:
+        f.write(response)
+
+
+def main():
+    c = dltk_ai.DltkAiClient('xxx')
+
+    response = c.face_detection_image('../img/fd-actual-img.jpg')  # it will return image data in bytes.
+    print(response)
+    save_img(response, "../img/fd-response.png")
+    response = c.face_detection_json('../img/fd-actual-img.jpg')  # it will return the co-ordinates of detected faces
+    print(response)
+    response = c.license_plate_detection_image('../img/lp-actual-img.jpg')  # it will return image data in bytes.
+    save_img(response, "../img/lp-response.png")
+    response = c.license_plate_detection_json(
+        '../img/lp-actual-img.jpg')  # it will return the co-ordinates of detected licence plates.
+    print(response)
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dltk_ai",
+    version="1.3.0",
+    author="DLTK",
+    author_email="connect@qubitai.tech",
+    description="Python Client for DLTK.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Spotflock/intellihub-sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
