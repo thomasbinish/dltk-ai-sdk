c1be7b6c8b76d896ec4195da3fa763a545b6acaa
dltk-ai <67465164+dltk-ai@users.noreply.github.com>
2020-07-21 13:16:09 +0530

    Update face_licence_plate_detection.py

--- python/examples/face_licence_plate_detection.py
+++ python/examples/face_licence_plate_detection.py
@@ -14,6 +14,16 @@ def main():
     save_img(response, "../img/fd-response.png")
     response = c.face_detection_json('../img/fd-actual-img.jpg')  # it will return the co-ordinates of detected faces
     print(response)
+    response = c.eye_detection_image('../img/fd-actual-img.jpg')  # it will return image data in bytes.
+    print(response)
+    save_img(response, "../img/fd-response.png")
+    response = c.eye_detection_json('../img/fd-actual-img.jpg')  # it will return the co-ordinates of detected faces
+    print(response)
+    response = c.smile_detection_image('../img/fd-actual-img.jpg')  # it will return image data in bytes.
+    print(response)
+    save_img(response, "../img/fd-response.png")
+    response = c.smile_detection_json('../img/fd-actual-img.jpg')  # it will return the co-ordinates of detected faces
+    print(response)
     response = c.license_plate_detection_image('../img/lp-actual-img.jpg')  # it will return image data in bytes.
     save_img(response, "../img/lp-response.png")
     response = c.license_plate_detection_json(
