1ca682f4bb514864be8044bb360d825d8aee7bbb
DLTK - Deep Learning Toolkit <connect@dltk.ai>
2020-07-24 22:21:31 +0530

    Update face_licence_plate_detection.py

--- python/examples/face_licence_plate_detection.py
+++ python/examples/face_licence_plate_detection.py
@@ -19,15 +19,26 @@ def main():
     save_img(response, "../img/fd-response.png")
     response = c.eye_detection_json('../img/fd-actual-img.jpg')  # it will return the co-ordinates of detected faces
     print(response)
-    response = c.smile_detection_image('../img/fd-actual-img.jpg')  # it will return image data in bytes.
+
+    response = c.face_detection_image_core('../img/fd-actual-img.jpg')  # it will return image data in bytes.
     print(response)
     save_img(response, "../img/fd-response.png")
-    response = c.smile_detection_json('../img/fd-actual-img.jpg')  # it will return the co-ordinates of detected faces
+    response = c.face_detection_json_core(
+        '../img/fd-actual-img.jpg')  # it will return the co-ordinates of detected licence plates.
     print(response)
+
     response = c.license_plate_detection_image('../img/lp-actual-img.jpg')  # it will return image data in bytes.
+    print(response)
     save_img(response, "../img/lp-response.png")
-    response = c.license_plate_detection_json(
+
+    response = c.object_detection_image('../img/lp-actual-img.jpg')  # it will return image data in bytes.
+    print(response)
+    save_img(response, "../img/lp-response.png")
+    response = c.object_detection_json(
         '../img/lp-actual-img.jpg')  # it will return the co-ordinates of detected licence plates.
+    print(response)
+
+    response = c.image_classification('../img/lp-actual-img.jpg')
     print(response)
 
 
